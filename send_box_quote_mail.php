92136f55aa1c00121cd8ce7e1ebefc76537c72e2
walterlobster <35208310+walterlobster@users.noreply.github.com>
2018-09-11 21:07:23 +0100

    Add files via upload

--- send_box_quote_mail.php
+++ send_box_quote_mail.php
@@ -1,38 +1,89 @@
-<?php
+﻿<?php
 $admin = "srus672@gmail.com";
 $admin_2 = "srus672@gmail.com";
-$height = $_POST['height'];
-$width = $_POST['width'];
-$depth = $_POST['depth'];
-$quantity = $_POST['quantity'];
-$print_color = $_POST['print_color'];
-$board_type = $_POST['board_type'];
-$outside_paper = $_POST['outside_paper'];
-$email = $_POST['email'];
-$grand_total = $_POST['grand_total'];
-$per_box_price = $_POST['per_box_price'];
-$price_with_vat = $_POST['price_with_vat'];
+$admin_2 = "srus672@gmail.com";
+$height = $_POST[height];
+$width = $_POST[width];
+$depth = $_POST[depth];
+$quantity = $_POST[quantity];
+$print_color = $_POST[print_color];
+$board_type = $_POST[board_type];
+$outside_paper = $_POST[outside_paper];
+$email = $_POST[email];
+$grand_total = $_POST[grand_total];
+$per_box_price = $_POST[per_box_price];
+$price_with_vat = $_POST[price_with_vat];
 $subject = "Box Cost Quote";
 $message = "<html>
 			<head>
-			<title>Box Cost Quote</title>
+			<title>SCD Bespoke Box Quote</title>
 			</head>
 			<body>
-        <p>
-          Here's the details of your box, including your quote.
+			<p align='left'> <img src='https://scdpackaging.co.uk/wp-content/uploads/2017/12/LOGO-80-1.png' width='80' height='82'> 
+		<h2>Your Custom Corrugated Box Quote</h2></p>
+		<p>Dear Prospective Valued Customer,</span>
+		</p>
+		<p>
+		  Thank you for completing our Online Box Calculator. We've specialised in bespoke cardboard boxes for over 15 years and provide the highest quality products and services whether your order is for a short run quantity, or a large multiple order. If you've never used SCD's Custom Box Services before, give us a try and find out why nearly every new SCD customer, go on to become loyal, long term clients.
         </p>
-				<p>
-          <b>Height:</b> $height <br>
-          <b>Width:</b> $width <br>
-          <b>Depth:</b> $depth <br>
-          <b>Quantity:</b> $quantity <br>
-          <b>Print Color:</b> $print_color <br>
-          <b>Board Type:</b> $board_type <br>
-          <b>Outside Paper:</b> $outside_paper <br>
-          <b>Price includes VAT:</b> $price_with_vat <br>
-          <h2><b>Grand Total:</b> &pound;$grand_total </h2>
-          <h4><b>Per Box Price:</b> &pound;$per_box_price </h4>
-				</p>
+		<p>
+          <i>Here's the information you requested from the SCD Packaging website for your bespoke box.</i>
+        </p>
+		 <p>
+          If you would like to discuss any aspect of this quotation, please reply to this email, or, call the SCD Offices. A summary of the quotation you requested below:
+        </p><br>
+			<p>
+				<table>
+					<table cellspacing=\"4\" cellpadding=\"4\" border=\"1\" align=\"center\">
+					<tr>
+					<td>Length:</td>
+					<td>$height</td>
+					</tr>
+					<tr>
+					<td>Width:</td>
+					<td>$width</td>
+					</tr>
+					<tr>
+					<td>Depth:</td>
+					<td>$depth</td>
+					</tr>
+					<tr>
+					<td>Quantity:</td>
+					<td>$quantity</td>
+					</tr>
+					<tr>
+					<td>Print Color:</td>
+					<td>$print_color</td>
+					</tr>
+					<tr>
+					<td>Board Type:</td>
+					<td>$board_type</td>
+					</tr>
+					<tr>
+					<td>Outside Paper:</td>
+					<td>$outside_paper</td>
+					</tr>
+					<tr>
+					<td><b>Grand Total:</b></td>
+					<td>$grand_total</td>
+					</tr>
+					<tr>
+					<td>Per Box Price:</td>
+					<td>$per_box_price</td>
+					</tr>
+					<td><b>Price includes VAT:</b></td>
+					<td>$price_with_vat</td>
+				</table>
+			</p>
+					<br>
+		<p>
+          This quotation is subject to confirmation, therefore, if you wish to purchase the boxes quoted above, <b>please call us on 01933 318 185.</b>
+        </p>
+		<p><b>
+		  Kind regards from the SCD Packaging Team</b>
+		</p>
+		<br>
+		<p align='left'> <img src='https://scdpackaging.co.uk/wp-content/uploads/2018/01/box-calc-footer.png' max-height: 218px; width='auto'> </p>
 			</body>
 			</html>";
 $headers  = 'MIME-Version:1.0' . "\r\n";
